34dca1dc5127834aa1f03908a95a550ec7c4e47d
kaliglass <146648840+kaliglass@users.noreply.github.com>
2024-04-28 18:28:55 +0530

    Added the functionalities for all the remaining buttons.

--- script.js
+++ script.js
@@ -3,19 +3,25 @@ const sub = (a,b) => a-b;
 const mul = (a,b) => a*b;
 const divi = (a,b) => a/b;
 
+function round(value, precision) {
+    var multiplier = Math.pow(10, precision || 0);
+    return Math.round(value * multiplier) / multiplier;
+}
+
 function operate(a,operation,b){
     switch (operation) {
         case '+':
-            return sum(a,b);
+            return round(sum(a,b),2);
             break;
         case '-':
-            return sub(a,b);
+            return round(sub(a,b),2);
             break;
         case '*':
-            return mul(a,b);
+            return round(mul(a,b),2);
             break; 
         case '/':
-            return divi(a,b);
+            if(b == 0) return alert("Dividing by zero is not good for health");
+            return round(divi(a,b),2);
             break;                   
     
         default:
@@ -30,30 +36,75 @@ const display = document.querySelector(".display");
 const cancel = document.querySelector(".cancel");
 const del  = document.querySelector(".del");
 const operation  = document.querySelectorAll(".operation");
+const equal = document.querySelector(".equal");
+const percent = document.querySelector(".percent");
+const deci = document.querySelector(".deci");
+percent.addEventListener("click",()=>{
+    op1 = Number(display.textContent);
+    display.textContent = operate(op1,"/",100);
+})
 
-let ope;
+
+let op1 = 0;
+let currentOperation = "";
+let op2 = 0;
+
+
 operation.forEach(button => {
     button.addEventListener("click",()=>{
-    let ope = button.textContent;
+    if(currentOperation === ""){
+    currentOperation = button.textContent;
+    op1 = Number(display.textContent);
+    display.textContent = "";
+    display.textContent.includes(".")?deci.disabled = true:deci.disabled = false;
+}
+    else{
+        op2 = Number(display.textContent);
+        display.textContent = operate(op1,currentOperation,op2);
+        currentOperation = button.textContent;
+        op1 = Number(display.textContent);
+        display.textContent.includes(".")?deci.disabled = true:deci.disabled = false;
+    }
 })
 });
 
-
+equal.addEventListener("click",()=>{
+    op2 = Number(display.textContent);
+    display.textContent = operate(op1,currentOperation,op2);
+    currentOperation = "";
+    display.textContent.includes(".")?deci.disabled = true:deci.disabled = false;
+})
 
 cancel.addEventListener("click",()=>{
-    display.textContent = ""
+    display.textContent = "";
+    op1 = undefined;
+    op2 = undefined;
+    currentOperation = "";
+    deci.disabled = false;
 });
 
 del.addEventListener("click",()=>{
     display.textContent= display.textContent.replace(/.$/, '')
+    display.textContent.includes(".")?deci.disabled = true:deci.disabled = false;
 });
 
 
 digits.forEach(button => {
     button.addEventListener("click",() => {
+        if(button.textContent == '.'){
+            deci.disabled = true;
+        }
+        if(Number(display.textContent) == op1){
+            display.textContent = button.textContent;
+            display.textcontent += button.textContent;
+        }
+        else{
         display.textContent += button.textContent;
+        }
+       
     })
 })
 
 
-// console.log(op1);
+
+
